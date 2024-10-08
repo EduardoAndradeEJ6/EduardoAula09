71773a7a19cd4bb1d9a8abb6163c457c13c213d8
EduardoAndradeEJ6 <contato.eduardo.ti@gmail.com>
2024-10-08 20:39:36 -0300

    exemplo 01

---
+++ exemplo1.py
@@ -0,0 +1,19 @@
+import os
+os.system('cls')
+
+ try:
+    n = int(input('Digite um número: '))
+except ValueError as e :
+
+
+
+
+
+
+
+# try:
+#     n = int(input('Digite um número: '))
+# except ValueError as e :
+#     print(f'{e}')
+# except KeyboardInterrupt as e :
+#     print(f'\n Operação cancelada pelo usuario')
