1bd68c86ae43cb613f55753a0fd662fb33c07433
EduardoAndradeEJ6 <contato.eduardo.ti@gmail.com>
2024-10-08 21:06:37 -0300

    Testes com try

--- exemplo1.py
+++ exemplo1.py
@@ -1,14 +1,20 @@
 import os
 os.system('cls')
 
- try:
-    n = int(input('Digite um número: '))
-except ValueError as e :
+# try:
+#     n = int(input('Digite um número: '))
+# except:
+#     print('Erro')
 
 
+try:
+    n = (input('Digite uma letra: '))[0]
+except (ValueError,KeyboardInterrupt) as e :
+    print(f'Erro {e}')
+else:
+    print(f'Você informou {n}')
 
-
-
+    
 
 
 # try:
@@ -17,3 +23,18 @@ except ValueError as e :
 #     print(f'{e}')
 # except KeyboardInterrupt as e :
 #     print(f'\n Operação cancelada pelo usuario')
+    
+
+
+
+
+# try: 
+#     txt = int(input("Informe o nome: "))
+# except IndexError as e : 
+#     print(f'Precisa digitar algo{e}: ')
+# else:
+#     print(f'Acertou')
+# finally:
+#     n = txt + 50
+
+#     print(f'A soma é {n}')
